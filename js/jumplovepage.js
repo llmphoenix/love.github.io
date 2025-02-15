31d471233396d75453f2a25b34e0600aaa4782e2
phoenixdementor <13004150501@126.com>
2025-02-15 21:38:22 +0800

    更新

--- js/jumplovepage.js
+++ js/jumplovepage.js
@@ -3,11 +3,25 @@ window.onload = initAll;//界面加载完毕后触发initAll函数
 
 function initAll()
 {
-    document.getElementById("redirect2love").onclick = initRedirect;//redirect元素被点击时触发initRedirect函数
+    document.getElementById("redirect2love").onclick = initRedirect; //redirect元素被点击时触发initRedirect函数
+    // document.getElementById("redirect2love2").onclick = initRedirect2;//redirect元素被点击时触发initRedirect函数
+    document.getElementById("redirect2love3").onclick = initRedirect3;//redirect元素被点击时触发initRedirect函数
 }
 
 function initRedirect()
 {
     window.location = "html/loveweb/index.html";//设置新页面
+    return false;//停止用户对点击的处理
+}
+
+function initRedirect2()
+{
+    window.location = "html/loveweb2/index.html";//设置新页面
+    return false;//停止用户对点击的处理
+}
+
+function initRedirect3()
+{
+    window.location = "html/loveweb3/index.html";//设置新页面
     return false;//停止用户对点击的处理
 }
